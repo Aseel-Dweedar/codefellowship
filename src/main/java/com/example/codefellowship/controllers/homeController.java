50a64d24155e11d566428d1453e8f076284337df
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-08-18 01:12:53 +0300

    lab 18 - 401

--- src/main/java/com/example/codefellowship/controllers/homeController.java
+++ src/main/java/com/example/codefellowship/controllers/homeController.java
@@ -9,7 +9,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,17 +26,27 @@ public class homeController {
     PostRepository postRepository;
 
     @GetMapping("/")
-    public String homePage(@AuthenticationPrincipal ApplicationUser user , Model model){
+    public String homePage(@AuthenticationPrincipal ApplicationUser user, Model model) {
 
-        if (user != null)model.addAttribute("username", applicationUserRepository.findByUsername(user.getUsername()).getUsername());
+        List<Post> postList = (List<Post>) postRepository.findAll();
 
-//        List<Post> postList = (List<Post>) postRepository.findAll();
-//        model.addAttribute("postList" , postList );
+        if (user != null) {
+            ApplicationUser currentUser = applicationUserRepository.findByUsername(user.getUsername());
+            model.addAttribute("username", currentUser.getUsername());
+
+            List<Post> myFollowingPost = new ArrayList();
+            for (Post post : postList) {
+                if (!currentUser.getFollowing().contains(post.getUser()) && post.getUser() != currentUser)  myFollowingPost.add(post);
+            }
+            model.addAttribute("postList", myFollowingPost);
+        } else {
+            model.addAttribute("postList", postList);
+        }
         return "home.html";
     }
 
     @GetMapping("/profile")
-    public String profile(@AuthenticationPrincipal ApplicationUser user , Model model){
+    public String profile(@AuthenticationPrincipal ApplicationUser user, Model model) {
         if (user != null) {
             ApplicationUser currentUser = applicationUserRepository.findByUsername(user.getUsername());
             model.addAttribute("userId", currentUser.getId());
@@ -42,10 +56,18 @@ public class homeController {
             model.addAttribute("dateOfBirth", currentUser.getDateOfBirth());
             model.addAttribute("bio", currentUser.getBio());
 
-            List<Post> postList =  postRepository.findAllByUser(currentUser);
-            model.addAttribute("postList" , postList );
+            List<Post> postList = postRepository.findAllByUser(currentUser);
+            model.addAttribute("postList", postList);
         }
         return "profile.html";
     }
 
+    @PostMapping("/follow")
+    public RedirectView printHi(@RequestParam Integer id, @AuthenticationPrincipal ApplicationUser user, Model model) {
+        ApplicationUser currentUser = applicationUserRepository.findByUsername(user.getUsername());
+        ApplicationUser newFollowing = applicationUserRepository.findById(id).get();
+        currentUser.setFollowing(newFollowing);
+        applicationUserRepository.save(currentUser);
+        return new RedirectView("/");
+    }
 }
